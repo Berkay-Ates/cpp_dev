e52488ff4e72a35920b20b44bccaeb083d93f75d
Berkay Ates <atesberkay2356@gmail.com>
2024-11-08 15:51:31 +0300

    initial commit

---
+++ leetcode_questions/08.11.2024/maks_xor.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <vector>
+ 
+using namespace std;
+
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int xs = 0;
+        for (int& x : nums) 
+            xs ^= x;
+
+        int n = nums.size();
+        vector<int> ans(n);
+        for (int i = 0; i < n; ++i) {
+            int x = nums[n - i - 1];
+            int k = 0;
+            for (int j = maximumBit - 1; ~j; --j) {
+                if ((xs >> j & 1) == 0) {
+                    k |= 1 << j;
+                }
+            }
+            ans[i] = k;
+            xs ^= x;
+        }
+        return ans;
+    }
+};
+ 
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
